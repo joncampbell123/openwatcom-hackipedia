ba278df9a939f758b12d948b910b68839d7df2d8
root <root@tvbox22.(none)>
2012-12-07 20:44:10 -0800

    iiiii

--- owconfig.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM *****************************************************************
-REM OWCONFIG.BAT - automatically generate batch file to set
-REM environment variables
-REM *****************************************************************
-
-REM All of the work is done in owconfig.vbs
-cscript -nologo owconfig.vbs %1 %2 %3 %4
